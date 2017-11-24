3231426e0c6943ca55fcdf8ada4095a2c007aeff
Moti Levy <moti@101tech.net>
2017-11-24 18:09:31 -0500

    test

--- ecs/ecs_cluster_info.py
+++ ecs/ecs_cluster_info.py
@@ -1,42 +1,48 @@
 #!/usr/bin/env python
-""" get some ecs info without needing a web browser """
-""" This script will take a cluster name and print out
-    all running tasks and the instances they run on
-    saving some browser clicks
-    """
-
 import sys
-import pprint
+# import pprint
 from collections import defaultdict
-import json
+# import json
 import boto3
+
+""" get some ecs info without needing a web browser
+This script will take a cluster name and print out
+all running tasks and the instances they run on
+saving some browser clicks
+"""
 
 
 def get_cluster_name():
     """ get cluster name from command arg """
     if len(sys.argv) < 2:
-        print "need something to connect to ... "
+        print "please provide cluster name"
         sys.exit(1)
     else:
         cluster = sys.argv[1]
+        print("looking up cluster {}".format(cluster))
     return cluster
+
 
 def list_clusters(client):
     """ get list of clusters from ecs """
     response = client.list_clusters()
     return response['clusterArns']
 
+
 def get_cluster(cluster, cluster_list):
     """ find the arn for the cluster we are looking for """
     for arn in cluster_list:
-        if cluster in arn:
-            short_name = arn.split('/')[1]
+        short_name = arn.split('/')[1]
+        if short_name == cluster:
             return arn, short_name
+    return None, None
+
 
 def list_services(client, arn):
     """ get services """
     response = client.list_services(cluster=arn)
     return response['serviceArns']
+
 
 def describe_services(client, arn, services, services_dict):
     """ describe services """
@@ -53,18 +59,19 @@ def describe_services(client, arn, services, services_dict):
             else:
                 services_dict[i['taskDefinition']]['elb'] = "None"
 
+
 def list_tasks(client, cluster_name, task_list):
     """ list tasks """
     response = client.list_tasks(cluster=cluster_name)
     for task_arn in response['taskArns']:
         task_list.append(describe_tasks(client, cluster_name, task_arn))
 
+
 def describe_tasks(client, cluster_name, arn):
     """ describe tasks """
     task_dict = defaultdict(dict)
     response = client.describe_tasks(cluster=cluster_name,
                                      tasks=[arn])
-    #print response
     task_info = response['tasks'][0]
     task_def_arn = task_info['taskDefinitionArn']
     instance_id = task_info['containerInstanceArn'].split('/')[1]
@@ -81,12 +88,14 @@ def describe_tasks(client, cluster_name, arn):
     task_dict[task_def_arn]['ami'] = ami
     return task_dict
 
+
 def list_instances(client, cluster_name):
     """ list tasks """
     response = client.list_container_instances(cluster=cluster_name)
     for instance_arn in response['containerInstanceArns']:
         instance = instance_arn.split('/')[1]
         describe_instances(client, cluster_name, instance)
+
 
 def get_instance_ip(instance):
     """ get the instance id """
@@ -99,12 +108,14 @@ def get_instance_ip(instance):
     iarn = instance_info['IamInstanceProfile']['Arn'].split('/')[1]
     return iip, itype, iami, iarn
 
+
 def describe_instances(client, cluster_name, instance):
     """ describe instances """
     response = client.describe_container_instances(cluster=cluster_name,
                                                    containerInstances=[instance])
     (iip, itype, iami, iarn) = get_instance_ip(response['containerInstances'][0]['ec2InstanceId'])
     return iip, itype, iami, iarn
+
 
 def print_service_info(service_arn, service_dict):
     """ print the service info """
@@ -115,6 +126,7 @@ def print_service_info(service_arn, service_dict):
     print "desired   : %s" % service_dict['desired']
     print "pending   : %s" % service_dict['pending']
     print "\n"
+
 
 def print_task_info(service_arn, task_dict):
     """ print the task info """
@@ -127,6 +139,7 @@ def print_task_info(service_arn, task_dict):
     print "instance arn : %s" % task_dict[service_arn]['instance_arn']
     print "\n"
 
+
 def main():
     """ let's do this """
     services_dict = defaultdict(dict)
@@ -136,20 +149,25 @@ def main():
     cluster = get_cluster_name()
     cluster_list = list_clusters(client)
     (arn, cluster_name) = get_cluster(cluster, cluster_list)
-
-    services = list_services(client, arn)
-    describe_services(client, arn, services, services_dict)
-
-    list_tasks(client, cluster_name, task_list)
-
-    for service_arn in services_dict.keys():
-        print "=" * 30
-        print_service_info(service_arn, services_dict[service_arn])
-        print "=" * 30
-        for task_dict in task_list:
-            if service_arn in task_dict:
-                print_task_info(service_arn, task_dict)
-
+    if arn is not None:
+        services = list_services(client, arn)
+        describe_services(client, arn, services, services_dict)
+        list_tasks(client, cluster_name, task_list)
+        for service_arn in services_dict.keys():
+            service_name = service_arn.split('/')[1]
+            service_name = service_name.split(':')[0]
+            print "=" * 30
+            print ("TASK INFO FOR SERVICE: {}".format(service_name))
+            print "=" * 30
+            print_service_info(service_arn, services_dict[service_arn])
+            for task_dict in task_list:
+                if service_arn in task_dict:
+                    print_task_info(service_arn, task_dict)
+    else:
+        print('could only find the following clusters in your region:')
+        for arn in cluster_list:
+            short_name = arn.split('/')[1]
+            print('cluster short name {}, arn {}'.format(short_name, arn))
 
 
 if __name__ == "__main__":
